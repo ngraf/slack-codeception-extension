6ac573c80c6f186d031f3c025bef79e222c6a64f
Norbert Graf <norbert.graf@verivox.com>
2016-12-30 11:25:26 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# slack-codeception-extension
-Provides an extension for Codeception to broadcast test results in Slack messenger
+slack-codeception-extension
+=============================
+This package provides an extension for Codeception to broadcast test results in Slack messenger
+
+Pre-requisites
+-------------
+
+- a pre-configured webhook from the Slack integration "Incoming Webhook"
+(see https://api.slack.com/incoming-webhooks for more information)
+
+Installation
+-----------
+
+Add the package `ngraf/slack-codeception-extension` to `composer.json` manually or type this in console:
+
+    composer require ngraf/slack-codeception-extension
+    
+Usage
+-----
+Enable and configure the extension in your `codeception.yaml`
+
+**Basic** usage:
+
+    extensions:
+      enabled:
+        - Codeception\Extension\SlackExtension
+    config:
+      Codeception\Extension\SlackExtension:
+         webhook: https://hooks.slack.com/services/...
+           
+**Advanced** usage:
+
+    extensions:
+       enabled:
+         - Codeception\Extension\SlackExtension
+    config:
+        Codeception\Extension\SlackExtension:
+           webhook:           https://hooks.slack.com/services/...
+           channel:           '#any-channel,@any-user'
+           username:          CI
+           icon:              :ghost:
+           messagePrefix:     '*Smoke-Test*'
+           messageSuffix:     <http://my-ci/my-job|Link>
+           
+           # possible notification strategies: always|failonly|failandrecover|statuschange
+           strategy:          always
+           
+           # If 'true' details about failed tests will be displayed. Default value: 'false'
+           extended:          true
+           
+           # Limit the size of error messages in extended mode. 0 = unlimited. Default value: 80
+           extendedMaxLength: 80
+           
+Example
+-----
+
+![slack-example](slack-example.png)
+
+Dependencies
+-----
+This package uses the package `maknz/slack` to communicate with the Slack API.
---
+++ src/Extension/SlackExtension.php
@@ -0,0 +1,254 @@
+<?php
+namespace Codeception\Extension;
+
+use Codeception\Exception\ExtensionException;
+use Maknz\Slack\Client;
+use Maknz\Slack\Message;
+
+/**
+ * This extension sends the test results to Slack channels.
+ *
+ * To use this extension a valid Slack Webhook URL is required.
+ *
+ * Configuration 'codeception.yaml' example:
+ *
+ *      extensions:
+ *        enabled:
+ *          - SlackExtension
+ *        config:
+ *          SlackExtension:
+ *            webhook: 'https://hooks.slack.com/services/...'
+ */
+class SlackExtension extends \Codeception\Extension
+{
+    const STRATEGY_ALWAYS = 'always';
+    const STRATEGY_FAIL_ONLY = 'failonly';
+    const STRATEGY_FAIL_AND_RECOVER = 'failandrecover';
+    const STRATEGY_STATUS_CHANGE = 'statuschange';
+
+    /**
+     * @var array list events to listen to
+     */
+    public static $events = array(
+        'result.print.after' => 'sendTestResults',
+    );
+
+    /**
+     * @var Message
+     */
+    protected $message;
+
+    /**
+     * @var string
+     */
+    protected $messagePrefix;
+
+    /**
+     * @var string
+     */
+    protected $messageSuffix;
+
+    /**
+     * @var array Array of slack channels
+     */
+    protected $channels;
+
+    /**
+     * @var boolean If set to true notifications will be send only when at least one test fails.
+     */
+    protected $strategy = self::STRATEGY_ALWAYS;
+
+    protected $strategies = array(
+        self::STRATEGY_ALWAYS, self::STRATEGY_FAIL_ONLY, self::STRATEGY_FAIL_AND_RECOVER, self::STRATEGY_STATUS_CHANGE
+    );
+
+    /**
+     * @var bool Whether or not to yet extended details about failed tests.
+     */
+    protected $extended = false;
+
+    /**
+     * @var bool
+     */
+    protected $lastRunFailed;
+
+    /**
+     * @var int Maximum length for error messages to be displayed in extended mode.
+     */
+    protected $extendedMaxLength = 80;
+
+    /**
+     * Setup Slack client and message object.
+     *
+     * @throws ExtensionException in case required configuration for 'webhook' is missing
+     */
+    public function _initialize()
+    {
+        if (!isset($this->config['webhook']) or empty($this->config['webhook'])) {
+            throw new ExtensionException($this, "SlackExtension requires a value for 'webhook'.");
+        }
+
+        $client = new Client($this->config['webhook']);
+
+        if (isset($this->config['channel'])) {
+            $this->channels = explode(',', $this->config['channel']);
+        }
+
+        if (isset($this->config['username'])) {
+            $client->setDefaultUsername($this->config['username']);
+        }
+
+        if (isset($this->config['icon'])) {
+            $client->setDefaultIcon($this->config['icon']);
+        }
+
+        if (isset($this->config['messagePrefix'])) {
+            $this->messagePrefix = $this->config['messagePrefix'] . ' ';
+        }
+
+        if (isset($this->config['messageSuffix'])) {
+            $this->messageSuffix = ' ' . $this->config['messageSuffix'];
+        }
+
+        if (isset($this->config['strategy'])) {
+            if (false === in_array(
+                    $this->config['strategy'],
+                    $this->strategies
+                )
+            ) {
+                throw new ExtensionException(
+                    $this,
+                    '"' . $this->config['strategy'] . '" is not a valid Slack notification "strategy".'
+                    . ' Possible values are: ' . PHP_EOL
+                    . implode(',', $this->strategies)
+                );
+            }
+            $this->strategy = $this->config['strategy'];
+        }
+
+        if (isset($this->config['extended'])
+            && (true === $this->config['extended'] || 'true' === $this->config['extended'])
+        ) {
+            $this->extended = true;
+        }
+
+        if (isset($this->config['extendedMaxLength'])) {
+            $this->extendedMaxLength = intval($this->config['extendedMaxLength']);
+        }
+
+        $this->lastRunFailed = $this->hasLastRunFailed();
+
+        $this->message = $client->createMessage();
+    }
+
+    /**
+     * Sends test results to Slack channels.
+     *
+     * This method is fired when the event 'result.print.after' occurs.
+     * @param \Codeception\Event\PrintResultEvent $e
+     */
+    public function sendTestResults(\Codeception\Event\PrintResultEvent $e)
+    {
+        $result = $e->getResult();
+
+        if ($result->wasSuccessful()) {
+
+            if (self::STRATEGY_ALWAYS === $this->strategy
+                || ($this->lastRunFailed && $this->strategy === self::STRATEGY_FAIL_AND_RECOVER)
+                || ($this->lastRunFailed && $this->strategy === self::STRATEGY_STATUS_CHANGE)
+            ) {
+                $this->sendSuccessMessage($result);
+            }
+
+        } else {
+
+            if (self::STRATEGY_ALWAYS === $this->strategy
+                || self::STRATEGY_FAIL_ONLY === $this->strategy
+                || self::STRATEGY_FAIL_AND_RECOVER === $this->strategy
+                || ($this->strategy === self::STRATEGY_STATUS_CHANGE && false === $this->lastRunFailed)
+            ) {
+                $this->sendFailMessage($result);
+            }
+        }
+    }
+
+    /**
+     * Sends success message to Slack channels.
+     *
+     * @param \PHPUnit_Framework_TestResult $result
+     */
+    private function sendSuccessMessage(\PHPUnit_Framework_TestResult $result)
+    {
+        $numberOfTests = $result->count();
+
+        foreach ($this->channels as $channel) {
+            $this->message->setChannel(trim($channel));
+            $this->message->send(
+                ':white_check_mark: '
+                . $this->messagePrefix
+                . $numberOfTests . ' of ' . $numberOfTests . ' tests passed.'
+                . $this->messageSuffix
+            );
+        }
+    }
+
+    /**
+     * Sends fail message to Slack channels.
+     *
+     * @param \PHPUnit_Framework_TestResult $result
+     */
+    private function sendFailMessage(\PHPUnit_Framework_TestResult $result)
+    {
+        $numberOfTests = $result->count();
+        $numberOfFailedTests = $result->failureCount() + $result->errorCount();
+
+        if (true === $this->extended) {
+            $this->attachExtendedInformation($this->message, $result);
+        }
+
+        foreach ($this->channels as $channel) {
+            $this->message->setChannel(trim($channel));
+
+            $this->message->send(
+                ':interrobang: '
+                . $this->messagePrefix
+                . $numberOfFailedTests . ' of ' . $numberOfTests . ' tests failed.'
+                . $this->messageSuffix
+            );
+        }
+    }
+
+    /**
+     *
+     * @param \PHPUnit_Framework_TestResult $result
+     */
+    private function attachExtendedInformation(Message &$message, \PHPUnit_Framework_TestResult $result) {
+        $fields = [];
+
+        foreach (array_merge($result->failures(), $result->errors()) as $failure) {
+            /**
+             * @var $failure \PHPUnit_Framework_TestFailure
+             */
+            $exceptionMsg = strtok($failure->exceptionMessage(), "\n");
+
+            if ($this->extendedMaxLength > 0  && strlen($exceptionMsg) > $this->extendedMaxLength) {
+                $exceptionMsg =  substr($exceptionMsg, 0, $this->extendedMaxLength) . ' ...';
+            }
+
+            $fields[] = [
+                'title' => $failure->getTestName(),
+                'value' => $exceptionMsg
+            ];
+        }
+
+        $message->attach([
+            'color' => 'danger',
+            'fields' => $fields
+        ]);
+    }
+
+    private function hasLastRunFailed()
+    {
+        return is_file($this->getLogDir() . 'failed');
+    }
+}
